92751b5fad5d49e15ea0f79c8a1a2724ed3c8e26
Vinato Many <many.vinatoooo@gmail.com>
2026-02-25 17:53:43 +0700

    fix error

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hello, World! %d");
+        System.out.printf("Hello, World! Successful Jenkins Test");
     }
 }
